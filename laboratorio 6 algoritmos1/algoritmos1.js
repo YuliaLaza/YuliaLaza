e91cc54c751dfaaf73bd8079e46106f92babbdc5
YuliaLaza <93838219+YuliaLaza@users.noreply.github.com>
2022-08-25 19:16:58 +0200

    Add files via upload

--- laboratorio 6 algoritmos1/algoritmos1.js
+++ laboratorio 6 algoritmos1/algoritmos1.js
@@ -84,7 +84,7 @@ var createProduct = product => {
     var input = document.createElement("input");
     container.appendChild(input);
     input.setAttribute("type", "number");
-    //input.setAttribute("value", 0);
+    input.setAttribute("value", 0);
     input.addEventListener("change", event => product.units = event.target.valueAsNumber);
 }
     var showProducts = productList => {
@@ -103,99 +103,33 @@ function totalPrice() {
   let total = 0;
   for (var product of products){
    total += product.price * product.units;
-  } console.log(total)
+  } //console.log(total)
   return total;
 };
 
-var handleButtonClick = () => console.log(totalPrice()); 
+function tax() {
+  let taxPaid = 0;
+  for (var product of products){
+    taxPaid += (product.price * product.units) * product.tax/100; 
+  } //console.log(tax)
+  return taxPaid;
+};
+
+function total() {
+var totPr = totalPrice();
+var iva = tax()
+var totalPay = totPr + iva;
+return totalPay;
+};
+
+
+var handleButtonClick = () => document.getElementById("subtotal").value = totalPrice(); 
+  document.getElementById("button").addEventListener("click", handleButtonClick);
+ 
+  var handleButtonClick = () => document.getElementById("impuestos").value = tax(); 
   document.getElementById("button").addEventListener("click", handleButtonClick);
 
-//    let lowerTax;
-//    let regularTax;
-//    let exemptTax;
-
-//   function calcTax() {
-//  for (item of products){
-
-//     if(item.tax == "LOWER_TYPE") {
-//           lowerTax += total * 0.04;
-  
-//         } else if(item.tax == "REGULAR_TYPE") {
-//          regularTax += total * 0,21;
-  
-//         } else {
-//           exemptTax += total * 1;
-//         }
-//       console.log(lowerTax + regularTax + exemptTax)
-//     } return lowerTax + regularTax + exemptTax;
-//   };
-  
-
-// // function totalPrice(){
-// //   var precio = unitsPrice();
-// //   var iva = calcTax();
-// //   var precioConIva = precio + iva;
-// //};
-
-// //  var handleButtonClick = () => console.log(calcTax());
-// //  document.getElementById("button").addEventListener("click", handleButtonClick);
+  var handleButtonClick = () => document.getElementById("total").value = total(); 
+  document.getElementById("button").addEventListener("click", handleButtonClick);
 
 
-
-
-//calculamos el precio total del producto, el iva y el total con iva
-
-// var tax = 0;
-
-// function calc() { //funcion principal
-
-//   var productPrice = () => { //funcion subtotal
-
-// for (var product of products) {
-   
-//    var price = products.units * products.price;
-
-//     for (var index = 0; index < products.length; index++) {
-    
-//         function subtotal(){
-//           var subtot = subtot + price; // sumamos los precios 
-//     }}
-//         }  
-//   } 
-//   var calcTax = () => { //CALCULA IVA  ((precio x cantidad) x iva)/100
-//     for (var product of products) {
-   
-//   if (products.tax === "LOWER_TYPE") {
-//     tax = tax + (product.price * product.units) * 0.04;
-//   } else if (product.tax === "REGULAR_TYPE") {
-//     tax = tax + (product.price * product.units) * 0,21;
-//   } else {
-//     tax = tax + (product.price * product.units);
-//   }
-  
-//   }
-//   for (var index = 0; index < products.length; index++) {
-// var totalConIva = () => {
-//     (productPrice + tax);
-
-// }}}}
-
-
-
-
-
-
-
-// //HACEMOS QUE EL BOTON CALCULE EL SUBTOTAL
-
-// var handleButtonClick = () => document.getElementById("subtotal").value = subtotal(); //aqui iria una funcion de calculo de total, subtotal, iva;
-// document.getElementById("button").addEventListener("click", handleButtonClick);
-
-
-// //Estoy enganchando al boton las funcion de total, subtota, iva a cada input correspondiente
-
-// var handleButtonClick = () => document.getElementById("impuestos").value = calcTax();
-// document.getElementById("button").addEventListener("click", handleButtonClick);
-
-// // var handleButtonClick = () => document.getElementById("total").value 
-// // document.getElementById("button").addEventListener("click", handleButtonClick);
